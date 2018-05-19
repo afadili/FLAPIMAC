374ccb6b9b3af13a780be543b0825e95a9b532ff
afadili <aminafadili@gmail.com>
2018-05-19 08:57:52 +0200

    prototypes fonctions de déplacements et actions player.h

--- src/player.c
+++ src/player.c
@@ -2,7 +2,22 @@
 
 /* Fichier de gestion du joueur (déplacements et actions) */
 
-// move player
-// move Up
-// move DOwn
-// shoot
+// Déplacement du Joueur vers le haut
+void moveUp(listEntite* player);
+
+// Déplacement du joueur vers le bas
+void moveDown(listEntite* player);
+
+// Déplacement des projectiles
+void moveProjectile(listProjectiles* projectile);
+
+// Déplacement des Bonus ( à rajouter plus tard)
+void moveBonus(listEntite* Bonus);
+
+// changement des proriétés du joueur selon les bonus récupérés
+void updateBonus(listEntite* player);
+
+// vérifie que la player ne sort pas de la fenetre, sinon le remet à sa place
+void positionPlayer(listEntite* player);
+
+// vitesse du player à gérer 
